f831067de1400c27e8edf713d36440f7b4979e46
Matheus Mota <matheusxmota@gmail.com>
2025-07-15 17:43:55 -0300

    Configure Next.js static export via next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Adicione esta linha aqui
 };
 
 export default nextConfig;
